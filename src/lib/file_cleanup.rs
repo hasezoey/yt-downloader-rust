c42d1ca58c214f704ead0ce425bd6f67230cdfeb
hasezoey <hasezoey@gmail.com>
2021-05-16 16:01:28 +0200

    style(file_cleanup): use "std::io::Result" over custom result

--- src/lib/file_cleanup.rs
+++ src/lib/file_cleanup.rs
@@ -1,9 +1,8 @@
 use super::utils::Arguments;
-use std::io::Error as ioError;
 
 use std::fs::remove_dir_all;
 
-pub fn file_cleanup(args: &Arguments) -> Result<(), ioError> {
+pub fn file_cleanup(args: &Arguments) -> std::io::Result<()> {
 	info!("Cleanup of tmp files");
 	return remove_dir_all(&args.tmp);
 }
