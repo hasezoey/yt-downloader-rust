11af0b4e9b5f2c873e8bf889a8e5fbefdf9ad8b2
hasezoey <hasezoey@gmail.com>
2021-05-16 14:40:37 +0200

    chore: add format & clippy scripts

---
+++ clippy.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# this file is an shorthand for the command below
+CLIPPY_DISABLE_DOCS_LINKS=1 cargo +nightly clippy --all-features -Z unstable-options "$@" -- -D clippy::correctness -W clippy::style -W clippy::complexity -W clippy::perf -A clippy::needless_return -D clippy::implicit_return
