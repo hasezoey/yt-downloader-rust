db1620b045958b713c9905dd903db97cd5676fd6
hasezoey <hasezoey@gmail.com>
2021-05-22 14:15:11 +0200

    feat: add module "paths"

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -4,6 +4,7 @@ pub mod errors;
 pub mod file_cleanup;
 pub mod import_archive;
 pub mod move_finished;
+pub mod paths;
 pub mod setup_archive;
 pub mod setup_arguments;
 pub mod spawn_main;
---
+++ src/lib/paths.rs
@@ -0,0 +1,104 @@
+use std::io::{
+	Error as ioError,
+	ErrorKind,
+	Result as ioResult,
+};
+use std::path::{
+	Component,
+	Path,
+	PathBuf,
+};
+
+/// This Function is copied from Cargo https://github.com/rust-lang/cargo/blob/070e459c2d8b79c5b2ac5218064e7603329c92ae/crates/cargo-util/src/paths.rs  
+/// The project might be licended under MIT  
+/// TODO: replace with official implementation when available  
+///
+/// Normalize a path, removing things like `.` and `..`.
+///
+/// CAUTION: This does not resolve symlinks (unlike
+/// [`std::fs::canonicalize`]). This may cause incorrect or surprising
+/// behavior at times. This should be used carefully. Unfortunately,
+/// [`std::fs::canonicalize`] can be hard to use correctly, since it can often
+/// fail, or on Windows returns annoying device paths. This is a problem Cargo
+/// needs to improve on.
+pub fn normalize_path(path: &Path) -> PathBuf {
+	let mut components = path.components().peekable();
+	let mut ret = if let Some(c @ Component::Prefix(..)) = components.peek().cloned() {
+		components.next();
+		PathBuf::from(c.as_os_str())
+	} else {
+		PathBuf::new()
+	};
+
+	for component in components {
+		match component {
+			Component::Prefix(..) => unreachable!(),
+			Component::RootDir => {
+				ret.push(component.as_os_str());
+			},
+			Component::CurDir => {},
+			Component::ParentDir => {
+				ret.pop();
+			},
+			Component::Normal(c) => {
+				ret.push(c);
+			},
+		}
+	}
+	return ret;
+}
+
+/// Convert `target` into an absolute path with `base` as a base.  
+/// If `target` is already absolute, it returns `target` as is.
+/// # Errors
+/// This Functions errors if `base` is not absolute with an [`ioError`]
+pub fn to_absolute(base: &Path, target: &Path) -> ioResult<PathBuf> {
+	if target.is_absolute() {
+		debug!("Path is already absolute");
+		return Ok(PathBuf::from(target));
+	}
+
+	if !base.is_absolute() {
+		return Err(ioError::new(ErrorKind::InvalidInput, "Base Path is not absolute!"));
+	}
+
+	return Ok(normalize_path(base.join(&target).as_ref()));
+}
+
+#[cfg(test)]
+mod test {
+	use super::*;
+
+	#[test]
+	fn test_normalize_path() {
+		let input_path = Path::new("hello/./../hello2/something");
+		assert_eq!(PathBuf::from("hello2/something"), normalize_path(&input_path));
+	}
+
+	#[test]
+	fn test_to_absolute() -> ioResult<()> {
+		let base_path = Path::new("/root/to/something");
+		let test_target_empty = Path::new("");
+		let test_target_1 = Path::new("/absolute/target");
+		let test_target_2 = Path::new("../../path/to/somewhere/./else");
+
+		// should return "base_path" unmodified
+		assert_eq!(PathBuf::from(&base_path), to_absolute(&base_path, &test_target_empty)?);
+		// should return "target" without "base_path" because its absolute
+		assert_eq!(PathBuf::from(&test_target_1), to_absolute(&base_path, &test_target_1)?);
+		// should return combined "base_path" and "target"
+		assert_eq!(
+			PathBuf::from("/root/path/to/somewhere/else"),
+			to_absolute(&base_path, &test_target_2)?
+		);
+
+		// should return an Error because base is not absolute
+		{
+			let returned = to_absolute(&test_target_2, &test_target_empty).unwrap_err();
+			assert_eq!(ErrorKind::InvalidInput, returned.kind());
+			assert_eq!("Base Path is not absolute!", returned.to_string());
+		}
+
+		return Ok(());
+	}
+}
