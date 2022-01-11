84eca9bf11961455d8d0fdb6294af0e5303aac66
hasezoey <hasezoey@gmail.com>
2022-01-11 18:42:21 +0100

    refactor: move old library files into "src/old/"

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -6,16 +6,8 @@ extern crate lazy_static;
 #[macro_use]
 extern crate log;
 
-pub mod archive_schema;
-pub mod ask_edit;
 mod error;
-pub mod file_cleanup;
-pub mod import_archive;
-pub mod move_finished;
-pub mod paths;
-pub mod setup_archive;
-pub mod setup_arguments;
+mod old;
 mod spawn;
-pub mod spawn_main;
-pub mod utils;
 pub use error::Error;
+pub use old::*;
---
+++ src/lib/old/mod.rs
@@ -0,0 +1,12 @@
+//! old library entry file
+
+pub mod archive_schema;
+pub mod ask_edit;
+pub mod file_cleanup;
+pub mod import_archive;
+pub mod move_finished;
+pub mod paths;
+pub mod setup_archive;
+pub mod setup_arguments;
+pub mod spawn_main;
+pub mod utils;
