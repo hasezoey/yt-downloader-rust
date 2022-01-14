507c2a023e378620095d50cf6f0ae8a280a77c62
hasezoey <hasezoey@gmail.com>
2022-01-14 02:07:10 +0100

    feat(spawn::ffmpeg): implement "ffmpeg_version"

--- src/lib/spawn/ffmpeg.rs
+++ src/lib/spawn/ffmpeg.rs
@@ -4,6 +4,8 @@ use std::process::{
 	Stdio,
 };
 
+use regex::Regex;
+
 #[inline]
 pub fn base_ffmpeg(overwrite: bool) -> Command {
 	let mut cmd = super::multiplatform::spawn_command("ffmpeg");
@@ -13,6 +15,10 @@ pub fn base_ffmpeg(overwrite: bool) -> Command {
 	}
 
 	return cmd;
+}
+
+lazy_static! {
+	static ref FFMPEG_VERSION_REGEX: Regex = Regex::new(r"(?mi)^ffmpeg version ([a-z0-9.-]+) Copyright").unwrap();
 }
 
 /// Get Version of `ffmpeg`
@@ -34,13 +40,55 @@ pub fn ffmpeg_version() -> Result<String, crate::Error> {
 		));
 	}
 
-	return Ok(String::from_utf8(command_output.stdout)?);
+	let as_string = String::from_utf8(command_output.stdout)?;
+
+	return ffmpeg_parse_version(&as_string);
 }
 
-pub fn ffmpeg_available() -> bool {
-	let ffmpeg_version = match ffmpeg_version() {
-		Err(_) => return false,
-		Ok(v) => v,
-	};
-	todo!()
+/// Internal Function to parse the input to a ffmpeg version with regex
+#[inline]
+fn ffmpeg_parse_version(input: &str) -> Result<String, crate::Error> {
+	return Ok(FFMPEG_VERSION_REGEX
+		.captures_iter(&input)
+		.next()
+		.ok_or_else(|| return crate::Error::NoCapturesFound("FFMPEG Version could not be determined".to_owned()))?[1]
+		.to_owned());
+}
+
+#[cfg(test)]
+mod test {
+	use super::ffmpeg_version;
+
+	#[test]
+	pub fn test_ffmpeg_parse_version_invalid_input() -> () {
+		assert_eq!(
+			super::ffmpeg_parse_version("hello"),
+			Err(crate::Error::NoCapturesFound(
+				"FFMPEG Version could not be determined".to_owned()
+			))
+		);
+	}
+
+	#[test]
+	pub fn test_ffmpeg_parse_version_valid_static_input() -> () {
+		let ffmpeg_output = "ffmpeg version n4.4.1 Copyright (c) 2000-2021 the FFmpeg developers
+built with gcc 11.1.0 (GCC)
+configuration: --prefix=/usr --disable-debug --disable-static --disable-stripping --enable-amf --enable-avisynth --enable-cuda-llvm --enable-lto --enable-fontconfig --enable-gmp --enable-gnutls --enable-gpl --enable-ladspa --enable-libaom --enable-libass --enable-libbluray --enable-libdav1d --enable-libdrm --enable-libfreetype --enable-libfribidi --enable-libgsm --enable-libiec61883 --enable-libjack --enable-libmfx --enable-libmodplug --enable-libmp3lame --enable-libopencore_amrnb --enable-libopencore_amrwb --enable-libopenjpeg --enable-libopus --enable-libpulse --enable-librav1e --enable-librsvg --enable-libsoxr --enable-libspeex --enable-libsrt --enable-libssh --enable-libsvtav1 --enable-libtheora --enable-libv4l2 --enable-libvidstab --enable-libvmaf --enable-libvorbis --enable-libvpx --enable-libwebp --enable-libx264 --enable-libx265 --enable-libxcb --enable-libxml2 --enable-libxvid --enable-libzimg --enable-nvdec --enable-nvenc --enable-shared --enable-version3
+libavutil      56. 70.100 / 56. 70.100
+libavcodec     58.134.100 / 58.134.100
+libavformat    58. 76.100 / 58. 76.100
+libavdevice    58. 13.100 / 58. 13.100
+libavfilter     7.110.100 /  7.110.100
+libswscale      5.  9.100 /  5.  9.100
+libswresample   3.  9.100 /  3.  9.100
+libpostproc    55.  9.100 / 55.  9.100
+";
+
+		assert_eq!(super::ffmpeg_parse_version(&ffmpeg_output), Ok("n4.4.1".to_owned()));
+	}
+
+	#[test]
+	pub fn test_ffmpeg_spawn() -> () {
+		assert!(ffmpeg_version().is_ok());
+	}
 }
