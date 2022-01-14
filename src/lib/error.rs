4876604fafee8b5841f21bbbebd30242f8d9c186
hasezoey <hasezoey@gmail.com>
2022-01-14 02:06:06 +0100

    feat(error): implement "PartialEq" and "Display"

--- src/lib/error.rs
+++ src/lib/error.rs
@@ -1,8 +1,25 @@
+use std::fmt::Display;
+
 /// Error type for "yt-downloader-rust", implements all Error types that could happen in this lib
+#[derive(Debug)]
 pub enum Error {
 	IoError(std::io::Error),
 	FromStringUTF8Error(std::string::FromUtf8Error),
 	CommandNotSuccesfull(String),
+	NoCapturesFound(String),
+}
+
+// this is custom, because "std::io::Error" does not implement "PartialEq", but "std::io::ErrorKind" does
+impl PartialEq for Error {
+	fn eq(&self, other: &Self) -> bool {
+		match (self, other) {
+			(Self::IoError(l0), Self::IoError(r0)) => l0.kind() == r0.kind(),
+			(Self::FromStringUTF8Error(l0), Self::FromStringUTF8Error(r0)) => l0 == r0,
+			(Self::CommandNotSuccesfull(l0), Self::CommandNotSuccesfull(r0)) => l0 == r0,
+			(Self::NoCapturesFound(l0), Self::NoCapturesFound(r0)) => l0 == r0,
+			(_, _) => false,
+		}
+	}
 }
 
 impl From<std::io::Error> for Error {
@@ -14,5 +31,20 @@ impl From<std::io::Error> for Error {
 impl From<std::string::FromUtf8Error> for Error {
 	fn from(err: std::string::FromUtf8Error) -> Self {
 		return Self::FromStringUTF8Error(err);
+	}
+}
+
+impl Display for Error {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		write!(
+			f,
+			"{}",
+			match &self {
+				Self::CommandNotSuccesfull(s) => format!("CommandNotSuccessfull: {}", s),
+				Self::NoCapturesFound(s) => format!("NoCapturesFound: {}", s),
+				Self::FromStringUTF8Error(v) => format!("FromStringUTF8Error: {}", v),
+				Self::IoError(v) => format!("IoError: {}", v),
+			}
+		)
 	}
 }
