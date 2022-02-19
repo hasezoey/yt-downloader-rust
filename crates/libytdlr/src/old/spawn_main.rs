51ba4d97db791e385948cbc1de54b2f2fbdce258
hasezoey <hasezoey@gmail.com>
2022-02-19 13:01:13 +0100

    style(spawn_main): apply clippy suggestions

--- crates/libytdlr/src/old/spawn_main.rs
+++ crates/libytdlr/src/old/spawn_main.rs
@@ -61,7 +61,7 @@ fn prefix_format<T: AsRef<str>>(current: &usize, count: &usize, id: T) -> String
 /// Spawn the main Youtube-dl task
 pub fn spawn_ytdl(args: &mut Arguments) -> Result<(), ioError> {
 	let count_video = crate::main_functionality::count(&args.url)
-		.map_err(|err| std::io::Error::new(ErrorKind::Other, format!("{}", err)))?
+		.map_err(|err| return std::io::Error::new(ErrorKind::Other, format!("{}", err)))?
 		.len();
 	let mut current_video: usize = 0;
 
