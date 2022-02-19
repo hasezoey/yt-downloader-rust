ded9e490fd7f64f04fed0451b786bd6d10d47eb2
hasezoey <hasezoey@gmail.com>
2022-02-19 12:13:15 +0100

    style(multiplatform::spawn_command): update docs to match what the function is doing

--- crates/libytdlr/src/spawn/multiplatform.rs
+++ crates/libytdlr/src/spawn/multiplatform.rs
@@ -2,14 +2,14 @@ use std::process::Command;
 
 // This file still exists and is seperated for future quick changes
 
-/// Spawn "youtube-dl" in non-windows / DOS systems
+/// Spawn a binary cross-system (not-windows version)
 #[cfg(not(target_os = "windows"))]
 #[inline]
 pub fn spawn_command(binary_name: &str) -> Command {
 	return Command::new(binary_name);
 }
 
-/// Spawn "youtube-dl" for windows / DOS systems
+/// Spawn a binary cross-system (windows version)
 /// Apparently, rust automatically adds a extensions (".exe") if none is specified
 /// Also, rust automatically searches all the paths, including the ytdl-rust binary path
 #[cfg(target_os = "windows")]
