ed375dae3341531674b4bfafff54fae55f90aadf
hasezoey <hasezoey@gmail.com>
2022-03-02 14:30:42 +0100

    fix(ytdlr::main): change to set new logger spec to only when verbosity is above 0

--- crates/ytdlr/src/main.rs
+++ crates/ytdlr/src/main.rs
@@ -46,22 +46,25 @@ fn main() -> Result<(), ioError> {
 
 	log::info!("CLI Verbosity is {}", cli_matches.verbosity);
 
-	// apply cli "verbosity" argument to the log level
-	logger_handle.set_new_spec(
-		match cli_matches.verbosity {
-			0 => LogSpecification::parse("warn"),
-			1 => LogSpecification::parse("info"),
-			2 => LogSpecification::parse("debug"),
-			3 => LogSpecification::parse("trace"),
-			_ => {
-				return Err(ioError::new(
-					std::io::ErrorKind::Other,
-					"Expected verbosity integer range between 0 and 3 (inclusive)",
-				))
-			},
-		}
-		.expect("Expected LogSpecification to parse correctly"),
-	);
+	// dont do anything if "-v" is not specified (use env / default instead)
+	if cli_matches.verbosity > 0 {
+		// apply cli "verbosity" argument to the log level
+		logger_handle.set_new_spec(
+			match cli_matches.verbosity {
+				0 => unreachable!("Unreachable because it should be tested before that it is higher than 0"),
+				1 => LogSpecification::parse("info"),
+				2 => LogSpecification::parse("debug"),
+				3 => LogSpecification::parse("trace"),
+				_ => {
+					return Err(ioError::new(
+						std::io::ErrorKind::Other,
+						"Expected verbosity integer range between 0 and 3 (inclusive)",
+					))
+				},
+			}
+			.expect("Expected LogSpecification to parse correctly"),
+		);
+	}
 
 	match &cli_matches.subcommands {
 		SubCommands::Download(v) => command_download(&cli_matches, v),
