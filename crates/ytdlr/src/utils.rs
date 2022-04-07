063de10e685bab79a593c31088f5731f4e93d0e2
hasezoey <hasezoey@gmail.com>
2022-04-07 15:29:04 +0200

    feat(main::utils): add function "handle_connect"

--- crates/ytdlr/src/utils.rs
+++ crates/ytdlr/src/utils.rs
@@ -1,15 +1,23 @@
 //! Utils for the `ytdlr` binary
 
 use crate::clap_conf::*;
+use diesel::SqliteConnection;
 use indicatif::{
 	ProgressBar,
 	ProgressDrawTarget,
 };
-use libytdlr::spawn::{
-	ffmpeg::ffmpeg_version,
-	ytdl::ytdl_version,
+use libytdlr::{
+	main::archive::import::ImportProgress,
+	spawn::{
+		ffmpeg::ffmpeg_version,
+		ytdl::ytdl_version,
+	},
 };
-use std::io::Error as ioError;
+use std::{
+	borrow::Cow,
+	io::Error as ioError,
+	path::Path,
+};
 
 /// Helper function to set the progressbar to a draw target if mode is interactive
 pub fn set_progressbar(bar: &ProgressBar, main_args: &CliDerive) {
@@ -46,4 +54,40 @@ pub fn require_ffmpeg_installed() -> Result<(), ioError> {
 	}
 
 	return Ok(());
+}
+
+/// Generic handler function for using [`libytdlr::main::sql_utils::migrate_and_connect`] with a [`ProgressBar`]
+pub fn handle_connect<'a>(
+	archive_path: &'a Path,
+	bar: &ProgressBar,
+	main_args: &CliDerive,
+) -> Result<(Cow<'a, Path>, SqliteConnection), libytdlr::Error> {
+	let pgcb_migrate = |imp| {
+		if main_args.is_interactive() {
+			match imp {
+				ImportProgress::Starting => bar.set_position(0),
+				ImportProgress::SizeHint(v) => bar.set_length(v.try_into().expect("Failed to convert usize to u64")),
+				ImportProgress::Increase(c, _i) => bar.inc(c.try_into().expect("Failed to convert usize to u64")),
+				ImportProgress::Finished(v) => bar.finish_with_message(format!("Finished Migrating {} elements", v)),
+				_ => (),
+			}
+		} else {
+			match imp {
+				ImportProgress::Starting => println!("Starting Migration"),
+				ImportProgress::SizeHint(v) => println!("Migration SizeHint: {}", v),
+				ImportProgress::Increase(c, i) => println!("Migration Increase: {}, Current Index: {}", c, i),
+				ImportProgress::Finished(v) => println!("Migration Finished, Successfull Migrations: {}", v),
+				_ => (),
+			}
+		}
+	};
+
+	let res = libytdlr::main::sql_utils::migrate_and_connect(archive_path, pgcb_migrate)?;
+
+	bar.println(format!(
+		"Migration from JSON to SQLite archive done, Archive path has changed to \"{}\"",
+		res.0.to_string_lossy()
+	));
+
+	return Ok(res);
 }
