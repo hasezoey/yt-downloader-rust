d152405029709177104bf48f0ef9f6ae29634e9b
hasezoey <hasezoey@gmail.com>
2022-04-23 16:16:35 +0200

    feat(lib::traits::context): update trait "DownloadOptions" for actual download function

--- crates/libytdlr/src/traits/context.rs
+++ crates/libytdlr/src/traits/context.rs
@@ -1,6 +1,11 @@
 //! Module for various Context traits
 
-use std::path::Path;
+use std::{
+	ffi::OsStr,
+	path::Path,
+};
+
+use diesel::SqliteConnection;
 
 /// Trait for Paths used by most top-level functions
 pub trait BasePaths {
@@ -14,24 +19,30 @@ pub trait BasePaths {
 	fn get_archive(&self) -> Option<&Path>;
 }
 
-/// Options specific for the [`download`] function
-/// TODO: replace link above to actual download function
+/// Options specific for the [`crate::main::download::download_single`] function
 pub trait DownloadOptions {
-	/// Get if the output should be audio-only
-	fn get_audio_only(&self) -> bool;
-	/// Get if Re-Thumbnailing should be enabled
-	fn get_re_thumbnail(&self) -> bool;
-	/// Get if Cleanup should be done afterwards
-	/// TODO: check if this is still needed, and if the name is correct
-	fn get_cleanup(&self) -> bool;
+	/// Get if the "audio-only" flag should be added
+	fn audio_only(&self) -> bool;
+	/// Get Extra Arguments that should be added to the ytdl command
+	fn extra_ytdl_arguments(&self) -> Vec<&OsStr>;
+	/// Get the path to where the Media should be downloaded to
+	fn download_path(&self) -> &Path;
+	/// Get a iterator over all the lines for a ytdl archive
+	/// All required videos should be made available with this function
+	/// For example if [`crate::main::count::count`] was used, it should be those matched against the SQL Archive, and those existing in both should be output
+	/// Otherwise, the whole SQL Archive could also be output, but may result in very large ytdl files
+	fn gen_archive<'a>(&'a self, connection: &'a mut SqliteConnection)
+		-> Option<Box<dyn Iterator<Item = String> + '_>>;
 	/// Get the URL to download
-	fn get_url(&self) -> &str;
+	fn get_url<'a>(&'a self) -> &'a str;
+	/// Get wheter or not to print out Command STDOUT (in this case ytdl)
+	/// STDERR is always printed (using [`log`])
+	/// With this returning `true`, the STDOUT output is also printed to [`log`], with [`log::trace`]
+	fn print_command_stdout(&self) -> bool;
+	/// Get a estimate of how many media elements will be downloaded
+	/// This commonly should be the length of the vec containing [`crate::main::count::CountVideo`] returned from [`crate::main::count::count`]
+	fn get_count_estimate(&self) -> usize;
 }
-
-/// Options specific for the [`import`] function
-/// Currently, there are no options specific for the "import" function
-/// TODO: replace link above to actual import function
-pub trait ImportOptions {}
 
 /// Options speicifc for the [`verify-archive`] function
 /// TODO: replace link above to actual verify-archive function
