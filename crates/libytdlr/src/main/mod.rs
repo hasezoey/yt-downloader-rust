711c38bd6110ffd78eb21af4ac0078202dd7b997
hasezoey <hasezoey@gmail.com>
2023-05-11 17:00:47 +0200

    feat(lib::main::count): remove module

--- crates/libytdlr/src/main/mod.rs
+++ crates/libytdlr/src/main/mod.rs
@@ -1,6 +1,5 @@
 //! Module for all the main functionality in the library (to keep everything sorted)
 pub mod archive;
-pub mod count;
 pub mod download;
 pub mod rethumbnail;
 // pub mod check_archive; // checkarchive command
