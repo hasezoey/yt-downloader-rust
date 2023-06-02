b112065fada20967821e18928e7a8cf4227913bb
hasezoey <hasezoey@gmail.com>
2023-06-02 16:59:55 +0200

    style(lib::error::ErrorInner): sort variants

--- crates/libytdlr/src/error.rs
+++ crates/libytdlr/src/error.rs
@@ -96,6 +96,17 @@ pub enum ErrorInner {
 	/// Wrapper Variant for [`std::string::FromUtf8Error`]
 	#[error("FromStringUTF8Error: {0}")]
 	FromStringUTF8Error(#[from] std::string::FromUtf8Error),
+	/// Variant for serde-json Errors
+	#[error("SerdeJSONError: {0}")]
+	SerdeJSONError(#[from] serde_json::Error),
+
+	/// Variant for a diesel Connection Error (sql i/o)
+	#[error("SQLConnectionError: {0}")]
+	SQLConnectionError(#[from] diesel::ConnectionError),
+	/// Variant for a diesel SQL Operation Error
+	#[error("SQLOperationError: {0}")]
+	SQLOperationError(#[from] diesel::result::Error),
+
 	/// Variant for when a spawned command was not successfull
 	#[error("CommandNotSuccessfull: {0}")]
 	CommandNotSuccesfull(String),
@@ -105,18 +116,9 @@ pub enum ErrorInner {
 	/// Variant for when a Unexpected EOF happened (like in import)
 	#[error("UnexpectedEOF: {0}")]
 	UnexpectedEOF(String),
-	/// Variant for serde-json Errors
-	#[error("SerdeJSONError: {0}")]
-	SerdeJSONError(#[from] serde_json::Error),
 	/// Variant for Other messages
 	#[error("Other: {0}")]
 	Other(String),
-	/// Variant for a diesel Connection Error (sql i/o)
-	#[error("SQLConnectionError: {0}")]
-	SQLConnectionError(#[from] diesel::ConnectionError),
-	/// Variant for a diesel SQL Operation Error
-	#[error("SQLOperationError: {0}")]
-	SQLOperationError(#[from] diesel::result::Error),
 }
 
 // this is custom, some errors like "std::io::Error" do not implement "PartialEq", but some inner type may do
@@ -125,14 +127,14 @@ impl PartialEq for ErrorInner {
 		match (self, other) {
 			(Self::IoError(l0), Self::IoError(r0)) => return l0.kind() == r0.kind(),
 			(Self::FromStringUTF8Error(l0), Self::FromStringUTF8Error(r0)) => return l0 == r0,
+			(Self::SQLConnectionError(l0), Self::SQLConnectionError(r0)) => return l0 == r0,
+			(Self::SQLOperationError(l0), Self::SQLOperationError(r0)) => return l0 == r0,
+
 			(Self::CommandNotSuccesfull(l0), Self::CommandNotSuccesfull(r0)) => return l0 == r0,
 			(Self::NoCapturesFound(l0), Self::NoCapturesFound(r0)) => return l0 == r0,
 			(Self::Other(l0), Self::Other(r0)) => return l0 == r0,
 			(Self::UnexpectedEOF(l0), Self::UnexpectedEOF(r0)) => return l0 == r0,
-			(Self::SQLConnectionError(l0), Self::SQLConnectionError(r0)) => return l0 == r0,
-			(Self::SQLOperationError(l0), Self::SQLOperationError(r0)) => return l0 == r0,
-			// Always return "false" for a serde_json::Error
-			(Self::SerdeJSONError(_l0), Self::SerdeJSONError(_r0)) => return false,
+
 			(_, _) => return false,
 		}
 	}
