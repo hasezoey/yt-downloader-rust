f2ecea82fc2dca0f7c6fca53d1b5dd7a1e433644
hasezoey <hasezoey@gmail.com>
2023-11-26 17:58:39 +0100

    chore: convert global clippy lints from script to "workspace.lints"

--- clippy.sh
+++ clippy.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 # this file is an shorthand for the command below
-CLIPPY_DISABLE_DOCS_LINKS=1 cargo +nightly clippy --all-features -Z unstable-options "$@" -- -D clippy::correctness -W clippy::style -W clippy::complexity -W clippy::perf -A clippy::needless_return -D clippy::implicit_return -A clippy::needless_doctest_main -A clippy::tabs_in_doc_comments
+CLIPPY_DISABLE_DOCS_LINKS=1 cargo +nightly clippy --all-features -Z unstable-options "$@" --
+# the following options have been transferred to /Cargo.toml#workspace.lints.clippy
+#-D clippy::correctness -W clippy::style -W clippy::complexity -W clippy::perf -A clippy::needless_return -D clippy::implicit_return -A clippy::needless_doctest_main -A clippy::tabs_in_doc_comments
