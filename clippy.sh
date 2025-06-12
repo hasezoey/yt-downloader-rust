6c8891f90e48d64cfd9392fc240f320c328171f8
hasezoey <hasezoey@gmail.com>
2025-06-12 14:43:17 +0200

    chore(clippy.sh): remove need for nightly

--- clippy.sh
+++ clippy.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 # this file is an shorthand for the command below
-CLIPPY_DISABLE_DOCS_LINKS=1 cargo +nightly clippy --all-features -Z unstable-options "$@" --
+cargo clippy --all-features "$@" --
 # the following options have been transferred to /Cargo.toml#workspace.lints.clippy
 #-D clippy::correctness -W clippy::style -W clippy::complexity -W clippy::perf -A clippy::needless_return -D clippy::implicit_return -A clippy::needless_doctest_main -A clippy::tabs_in_doc_comments
+
+# the following options were also enabled, but are not necessary anymore
+# CLIPPY_DISABLE_DOCS_LINKS=1
+# -Z unstable-options
+# +nightly
